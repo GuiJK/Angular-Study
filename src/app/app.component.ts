794cedd4b155ad321a968c80ade6a5ba9529b135
GuiJK <junquekarabe18@gmail.com>
2022-09-22 13:44:01 -0300

    Renderização condicional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Mayla';
+
+  userData= {
+    email: 'junquekarabe18@gmail.com',
+    role: 'Admin',
+  };
+
   title = 'curso-angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentsDataComponent } from './components/parents-data/parents-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ParentsDataComponent,
+    DirectivesComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule,
